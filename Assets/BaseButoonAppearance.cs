75f25c89db16e2075fe6485855b1e9c8ed5ef9f4
Evangelos Karvounis <evangeloskarvounis@Evangeloss-MacBook-Pro.local>
2020-02-01 14:46:44 +0200

    first commit

---
+++ Assets/BaseButoonAppearance.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BaseButoonAppearance : MonoBehaviour
+{
+	public string label;
+
+    public Color color;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponentInChildren<TextMeshPro>().text = this.label;
+        this.gameObject.GetComponentInChildren<SpriteRenderer>().color = new Color(this.color.r,this.color.g,this.color.b);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
